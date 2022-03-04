0fc6eeadf57e219921b9ec8cfeecb65fd5a9106c
obj-QED <obj.qed@gmail.com>
2022-03-04 17:55:42 +0200

    Функции Update

--- script/script.js
+++ script/script.js
@@ -8,29 +8,30 @@ function showFirstMessage(text) {
     let num = 10;
     console.log(num);
 }
-showFirstMessage('Hello World');
+showFirstMessage("Hello World");
 
 function calc(a, b) {
-    return (a + b);
+    return a + b;
 }
 console.log(calc(3, 4));
 
 function ret() {
     let num = 50;
-    return num;
+    return num; // результат выполнения функции
 }
 const anitherNum = ret();
 console.log(anitherNum);
 
 // Function Expression
-
-const logger = function() {
-    console.log('Hello');
+const logger = function () {
+    console.log("Hello");
 };
 logger();
 
 // Arrow Function
 
-const calc = (a, b) => {
+const calcArrow = (a, b) => {
     return a + b;
 };
+// or
+const calcArrow2 = (a, b) => a + b;
